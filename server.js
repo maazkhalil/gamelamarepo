e72d9c68de8b0b59088531585f28213ce5a3d4eb
maaz khalil <maazkhalil500@gmail.com>
2025-01-29 15:37:49 +0500

    localhost

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.post("/contact", async (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-    console.log(`Server running on port ${process.env.PORT || 5000}`);
+  console.log(`Server running on http://localhost:${PORT}`);
+    // console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
